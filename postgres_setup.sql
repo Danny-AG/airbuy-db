f1f653734f9b151e3338bd9cf6f68aebb5d893e8
Danny Gaspar <dannygaspar.dg@gmail.com>
2023-08-19 12:49:43 +0100

    Define schemas for global sqm, airbnb and area conversion tables

---
+++ postgres_setup.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE airbuy;
+
+\connect airbuy
+
+CREATE TABLE global_property_sqm_purchase_prices
+(
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    avg_sqm_price INTEGER NOT NULL,
+    data_source_url VARCHAR(255) NOT NULL,
+    data_source_date DATE NOT NULL,
+    PRIMARY KEY (city, country)
+);
+
+CREATE TABLE airbnb_rental_prices
+(
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    num_bedrooms INTEGER NOT NULL,
+    entry_count INTEGER NOT NULL,
+    avg_price_per_night INTEGER NOT NULL,
+    data_source_url VARCHAR(255) NOT NULL,
+    data_source_date DATE NOT NULL,
+    PRIMARY KEY (city, country, num_bedrooms)
+);
